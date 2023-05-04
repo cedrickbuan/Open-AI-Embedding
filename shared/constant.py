741e1bf7582e5b84f928022541589909263a3fe0
ceds <cedrickbuan@gmail.com>
2023-05-04 21:52:57 +0800

    updated constant url

--- shared/constant.py
+++ shared/constant.py
@@ -1,4 +1,6 @@
 class Constant:
     URLS = {
-        'CHATGPT_QUESTION': '/chatgpt/question'
+        'CHATGPT_QUESTION': '/chatgpt/question',
+        'CHATGPT_IS_TEXT_POSITIVE': '/chatgpt/sentiment/checkifpositive',
+        'CHATGPT_IS_IMAGE_POSITIVE': '/chatgpt/sentiment/checkifpositiveImage'
     }
